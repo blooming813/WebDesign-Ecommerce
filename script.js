1398ad28b8bebf0653ed9b5e854219cc6f4f5396
blooming813 <blooming.anna0813@gmail.com>
2024-05-24 15:32:45 -0400

    index done

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const bar = document.getElementById('bar');
 const nav = document.getElementById('navbar');
+const cls = document.getElementById('close');
+const lgCart = document.getElementById('lg-cart');
 
 if (bar) {
     bar.addEventListener('click',()=>{
+        lgCart.classList.add('hidden');
         nav.classList.add('active');
         console.log('works')
     })
 }
+
+if (cls) {
+    cls.addEventListener('click', ()=>{
+        nav.classList.remove('active');
+    })
+}
+
